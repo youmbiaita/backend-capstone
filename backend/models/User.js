0283b91ccf7c82bdfe9913aed6d62a23b8d540af
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-06-25 21:36:25 -0400

    delete admin and password

--- backend/models/User.js
+++ backend/models/User.js
@@ -11,26 +11,8 @@ const userSchema = new Schema({
     type: String, 
     required: true, 
     unique: true
-   },
-  password: {
-    type: String
-   
-  },
-  isAdmin: {
-    type: Boolean
-  }
+   }
   
 });
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) {
-//     return next();
-//   }
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
-
-// userSchema.methods.comparePassword = function (password) {
-//   return bcrypt.compare(password, this.password);
-// };
 
 module.exports = model('User', userSchema);
--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-// const { Router } = require('express');
-// const authCtrl = require('../controllers/auths');
-
-// const router = Router();
-
-// router.post('/login', authCtrl.login);
-
-// module.exports = router;
